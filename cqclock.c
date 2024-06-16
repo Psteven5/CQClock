e1e2dd2a7606662c5d7be2afb617d473f6129f7d
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-06-16 23:00:37 +0200

    Got rid of puts
    there was one puts that was there because of space reasons, but that is not necessary anymore as I moved the cursor move escape code inside of another printf saving space in the process

--- cqclock.c
+++ cqclock.c
@@ -5,9 +5,9 @@ asm("f:.incbin \""__FILE__"\",0\n.byte 0");main
 (){for(;;){time(&c);struct tm*t=localtime(&c);h
 =t->tm_hour;m=t->tm_min;s=t->tm_sec;*a=h/10;a[1
 ]=h%10;a[3]=m/10;a[4]=m%10;a[6]=s/10;a[7]=s%10;
-extern char f[];char*g=f;puts("\033[35m");for(y
-=5;y>0;--y){for(i=0;i<8;++i,++g){for(x=5;x>0;--
-x,++g)if((n[a[i]]>>((x-1)+5*(y-1)))&1)printf("\
-\033[46m%c\033[40m",*g);else printf("%c",*g);if
-(13==*g)++g;printf("%c",*g);}}printf("\033[34m\
-%s",g); printf("\033[0m\033[14A"); sleep(1); }}
+extern char f[];char*g=f;printf("\033[35m");for
+(y=5;y>0;--y){for(i=0;i<8;++i,++g){for(x=5;x>0;
+--x,++g)if((n[a[i]]>>((x-1)+5*(y-1)))&1)printf(
+"\033[46m%c\033[40m",*g); else printf("%c",*g);
+if (13 == *g) ++g; printf("%c", *g); }} printf(
+"\033[34m%s\033[0m\033[13A", g); sleep(1);  } }
